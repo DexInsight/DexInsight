9adfa712d18606aa161ef8733a7c2d29e9db70d9
Hunter99011 <hothertech@gmail.com>
2024-05-13 07:50:39 +0200

    Add ThemeViewModel for the Settings Theme Selection

---
+++ desktop/DexInsights/ViewModels/ThemeViewModel.cs
@@ -0,0 +1,22 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DexInsights.ViewModels {
+    public partial class ThemeViewModel : ObservableObject {
+
+        [ObservableProperty]
+        Color themeColor;
+
+        public ThemeViewModel() {
+            this.themeColor = Color.FromArgb("CC0D8921");
+        }
+
+        public void ChangeColor(Color color) {
+            this.ThemeColor = color;
+        }
+    }
+}
