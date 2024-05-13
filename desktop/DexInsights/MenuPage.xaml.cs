20faae04a92357901dfbed114b99a4aa9fd568f4
Hunter99011 <hothertech@gmail.com>
2024-05-13 07:49:50 +0200

    Create base MenuPage layout

---
+++ desktop/DexInsights/MenuPage.xaml.cs
@@ -0,0 +1,41 @@
+using DexInsights.ViewModels;
+using DexInsights.Views;
+
+namespace DexInsights;
+
+public partial class MenuPage : ContentPage {
+    private ThemeViewModel tvm;
+    private string selected = "";
+    public MenuPage(ThemeViewModel model)
+	{
+		InitializeComponent();
+        BindingContext = model;
+        this.tvm = model;
+        NavigationPage.SetHasBackButton(this, false);
+        NavigationPage.SetHasNavigationBar(this, false);
+    }
+
+    private void HoverEndedMenuButton(object sender, PointerEventArgs e) {
+        var animation = new Animation {
+        { 0, 1, new Animation(v => ((Frame)sender).BackgroundColor = Color.FromRgba(48, 138, 56, v), 0.3, 0) },
+        { 0, 1, new Animation(v => ((Frame)sender).TranslationX = v, -20, 0) }
+    };
+
+        animation.Commit(((Frame)sender), "AnimateColorAndTranslation", length: 250);
+    }
+    private void HoverBeganMenuButton(object sender, PointerEventArgs e) {
+        var animation = new Animation {
+            { 0, 1, new Animation(v => ((Frame)sender).BackgroundColor = Color.FromRgba("8a8a8a99"), start: 0, end: 10000) },
+            { 0, 1, new Animation(v => ((Frame)sender).TranslationX = v, 0, -20) }
+        };
+
+        animation.Commit(((Frame)sender), "AnimateColorAndTranslation", length: 250);
+    }
+
+    private void SettingsButtonClicked(object sender, TappedEventArgs e) {
+        if (selected != "settings") {
+            ContentPane.Children.Add(new SettingsView(tvm));
+            selected = "settings";
+        }
+    }
+}
