dc74c6c89565c544e65fb528589e6de5aaf33c53
Hunter99011 <hothertech@gmail.com>
2024-05-13 07:47:36 +0200

    Create SettingsView layout for the menu

---
+++ desktop/DexInsights/Views/SettingsView.xaml.cs
@@ -0,0 +1,29 @@
+using DexInsights.ViewModels;
+
+namespace DexInsights.Views;
+
+public partial class SettingsView : ContentView
+{
+    private ThemeViewModel tvm;
+	public SettingsView(ThemeViewModel model)
+	{
+		InitializeComponent();
+        BindingContext = model;
+        this.tvm = model;
+
+        AnimateAppearing();
+	}
+
+    private void ChangeThemeColor(object sender, TappedEventArgs e) {
+        Border? border = sender as Border;
+        tvm.ChangeColor(border.BackgroundColor);
+    }
+
+    private void AnimateAppearing() {
+        var animation = new Animation {
+        { 0, 1, new Animation(v => ((Frame)ContentFrame).TranslationY = v, 200, 0) }
+    };
+
+        animation.Commit(((Frame)ContentFrame), "AnimateTranslation", length: 200);
+    }
+}
