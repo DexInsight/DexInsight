2eb5fdfd4c6247c37167b24940c83367f9591171
Hunter99011 <hothertech@gmail.com>
2024-05-23 17:20:30 +0200

    Add darker gradient to the menu theme

--- desktop/DexInsights/ViewModels/ThemeViewModel.cs
+++ desktop/DexInsights/ViewModels/ThemeViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.Maui.Animations;
+using SkiaSharp;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,13 +12,20 @@ namespace DexInsights.ViewModels {
 
         [ObservableProperty]
         Color themeColor;
+        [ObservableProperty]
+        Color themeColor2;
 
         public ThemeViewModel() {
-            this.themeColor = Color.FromArgb("CC0D8921");
+            this.themeColor = Color.FromArgb("CC890D0D");
+            this.themeColor2 = Darken(themeColor);
         }
 
         public void ChangeColor(Color color) {
             this.ThemeColor = color;
+            this.ThemeColor2 = Darken(color);
+        }
+        public Color Darken(Color color) {
+            return color.WithLuminosity((float)(color.GetLuminosity() - (color.GetLuminosity() * .4)));
         }
     }
 }
