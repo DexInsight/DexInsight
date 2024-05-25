4f0389f7d1027422f2003cc0ae372872e6fb0432
Hunter99011 <hothertech@gmail.com>
2024-05-25 18:13:43 +0200

    Add Lighten for themeColor3

--- desktop/DexInsights/ViewModels/ThemeViewModel.cs
+++ desktop/DexInsights/ViewModels/ThemeViewModel.cs
@@ -14,18 +14,26 @@ namespace DexInsights.ViewModels {
         Color themeColor;
         [ObservableProperty]
         Color themeColor2;
+        [ObservableProperty]
+        Color themeColor3;
 
         public ThemeViewModel() {
             this.themeColor = Color.FromArgb("CC890D0D");
-            this.themeColor2 = Darken(themeColor);
+            this.themeColor2 = Darken(themeColor, 0.4);
+            this.themeColor3 = Darken(themeColor, 0.1);
         }
 
         public void ChangeColor(Color color) {
             this.ThemeColor = color;
-            this.ThemeColor2 = Darken(color);
+            this.ThemeColor2 = Darken(color, 0.4);
+            this.ThemeColor3 = Darken(color, 0.1);
         }
-        public Color Darken(Color color) {
-            return color.WithLuminosity((float)(color.GetLuminosity() - (color.GetLuminosity() * .4)));
+        public Color Darken(Color color, double level) {
+            return color.WithLuminosity((float)(color.GetLuminosity() - (color.GetLuminosity() * level)));
+        }
+
+        public Color Lighten(Color color, double level) {
+            return color.WithLuminosity((float)(color.GetLuminosity() + (color.GetLuminosity() * level)));
         }
     }
 }
