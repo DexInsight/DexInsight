1cfa75b13b9ed7475f5bd070716890a4544124f9
Hunter99011 <hothertech@gmail.com>
2024-05-25 18:10:50 +0200

    Temporary local setup for future User Database actions

---
+++ desktop/DexInsights/Database/ManagementHandler.cs
@@ -0,0 +1,24 @@
+﻿using DexInsights.DataModels;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DexInsights.Database {
+    internal class ManagementHandler {
+        public static List<DbUser> GetUsers() {
+            string json = File.ReadAllText(Path.Combine(AppContext.BaseDirectory, "..\\..\\..\\..\\..\\Database\\Users.json"));
+            JArray usersArray = JArray.Parse(json);
+            List<DbUser> usersList = usersArray.ToObject<List<DbUser>>();
+
+            return usersList;
+        }
+        public static void SaveUsers(List<DbUser> users) {
+            string json = JsonConvert.SerializeObject(users, Formatting.Indented);
+            File.WriteAllText(Path.Combine(AppContext.BaseDirectory, "..\\..\\..\\..\\..\\Database\\Users.json"), json);
+        }
+    }
+}
