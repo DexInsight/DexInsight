5f66d1ec6d411566a8b44fefc254f96cbd1c7a99
Hunter99011 <hothertech@gmail.com>
2024-05-26 14:15:14 +0200

    Add animated appearing for Fields View

--- desktop/DexInsights/Views/FieldsView.xaml.cs
+++ desktop/DexInsights/Views/FieldsView.xaml.cs
@@ -11,5 +11,13 @@ public partial class FieldsView : ContentView
         BindingContext = model;
         this.tvm = model;
 
+        AnimateAppearing();
+    }
+
+    private void AnimateAppearing() {
+        var animation = new Animation {
+        { 0, 1, new Animation(v => ((Frame)ContentFrame).TranslationY = v, 200, 0) }
+    };
+        animation.Commit(((Frame)ContentFrame), "AnimateTranslation", length: 200);
     }
 }
