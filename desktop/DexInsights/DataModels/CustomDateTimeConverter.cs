e27bd473e07ee79ae53963799ab88c5b9eae02d1
Hunter99011 <hothertech@gmail.com>
2024-05-26 12:21:29 +0200

    Add Custom DateTime converter for the House date_sold and data_added parse

---
+++ desktop/DexInsights/DataModels/CustomDateTimeConverter.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DexInsights.DataModels {
+    public class CustomDateTimeConverter : IsoDateTimeConverter {
+        public CustomDateTimeConverter() {
+            DateTimeFormat = "yyyy-MM-dd";
+        }
+    }
+}
