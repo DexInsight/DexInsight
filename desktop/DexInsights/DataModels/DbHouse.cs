94c239974e35f90f390c113cb8e4196976baa1e2
Hunter99011 <hothertech@gmail.com>
2024-05-26 12:21:00 +0200

    Add House Object and corresponding DbReader

---
+++ desktop/DexInsights/DataModels/DbHouse.cs
@@ -0,0 +1,78 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DexInsights.DataModels {
+    public class DbHouse {
+        [JsonProperty("date_added")]
+        [JsonConverter(typeof(CustomDateTimeConverter))]
+        private DateTime date_added { get; set; }
+
+        [JsonProperty("date_sold")]
+        [JsonConverter(typeof(CustomDateTimeConverter))]
+        private DateTime date_sold { get; set; }
+
+        [JsonProperty("boughtBy")]
+        private List<string> boughtBy { get; set; }
+
+        [JsonProperty("price")]
+        private int price { get; set; }
+
+        [JsonProperty("size")]
+        private int size { get; set; }
+
+        [JsonProperty("county")]
+        private string county { get; set; }
+
+        [JsonProperty("city")]
+        private string city { get; set; }
+
+        [JsonProperty("address")]
+        private List<string> address { get; set; }
+
+        public DbHouse(DateTime date_added, DateTime date_sold, List<string> boughtBy, int price, int size, string county, string city, List<string> address) {
+            this.date_added = date_added;
+            this.date_sold = date_sold;
+            this.boughtBy = boughtBy;
+            this.price = price;
+            this.size = size;
+            this.county = county;
+            this.city = city;
+            this.address = address;
+        }
+
+        public DateTime GetDateAdded() {
+            return this.date_added;
+        }
+        public DateTime GetDateSold() {
+            return this.date_sold;
+        }
+
+        public List<string> GetBoughtBy() {
+            return this.boughtBy;
+        }
+
+        public int GetPrice() {
+            return this.price;
+        }
+
+        public int GetSize() {
+            return this.size;
+        }
+
+        public string GetCounty() {
+            return this.county;
+        }
+
+        public string GetCity() {
+            return this.city;
+        }
+
+        public List<string> GetAddress() {
+            return this.address;
+        }
+    }
+}
--- desktop/DexInsights/Database/ManagementHandler.cs
+++ desktop/DexInsights/Database/ManagementHandler.cs
@@ -16,9 +16,23 @@ namespace DexInsights.Database {
 
             return usersList;
         }
+
         public static void SaveUsers(List<DbUser> users) {
             string json = JsonConvert.SerializeObject(users, Formatting.Indented);
             File.WriteAllText(Path.Combine(AppContext.BaseDirectory, "..\\..\\..\\..\\..\\Database\\Users.json"), json);
+        }
+
+        public static List<DbHouse> GetHouses() {
+            string json = File.ReadAllText(Path.Combine(AppContext.BaseDirectory, "..\\..\\..\\..\\..\\Database\\Houses.json"));
+            JArray housesArray = JArray.Parse(json);
+            List<DbHouse> housesList = housesArray.ToObject<List<DbHouse>>();
+
+            return housesList;
+        }
+
+        public static void SaveHouses(List<DbHouse> houses) {
+            string json = JsonConvert.SerializeObject(houses, Formatting.Indented);
+            File.WriteAllText(Path.Combine(AppContext.BaseDirectory, "..\\..\\..\\..\\..\\Database\\Houses.json"), json);
         }
     }
 }
