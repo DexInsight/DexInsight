93c153b15c0286aa0979412e0f421af88989d5ec
Hunter99011 <hothertech@gmail.com>
2024-05-27 01:57:30 +0200

    Add RevertEdit Message for TableField communication

---
+++ desktop/DexInsights/Messages/RevertEditFieldMessage.cs
@@ -0,0 +1,13 @@
+﻿using CommunityToolkit.Mvvm.Messaging.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DexInsights.Messages {
+    public class RevertEditFieldMessage : ValueChangedMessage<int> {
+    public RevertEditFieldMessage(int value) : base(value) {
+    }
+}
+}
